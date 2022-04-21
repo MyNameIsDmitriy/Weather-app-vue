05f68d79cc6e86c6e8b2104970bf30e406b614c0
MyNameIsDmitriy <gopanchuk.dm@mail.ru>
2022-04-21 12:47:45 +0300

    download weather SVGs and set up key-value in object

--- src/weatherImgObject.js
+++ src/weatherImgObject.js
@@ -1,12 +1,18 @@
 const weatherImgObject = {
+  "Light rain": require("@/assets/svg/rain.svg"),
+  "Light drizzle": require("@/assets/svg/overcast-drizzle.svg"),
+  "Light rain shower": require("@/assets/svg/rain.svg"),
+  "Patchy rain possible": require("@/assets/svg/light-drizzle.svg"),
+  "Moderate rain": require("@/assets/svg/rain.svg"),
+  "Heavy rain": require("@/assets/svg/extreme-rain.svg"),
   Fog: require("@/assets/svg/fog.svg"),
-  "Light rain": require("@/assets/svg/fog.svg"),
-  "Light drizzle": require("@/assets/svg/light-drizzle.svg"),
-  Overcast: require("@/assets/svg/thunderstorms-extreme-rain.svg"),
+  Mist: require("@/assets/svg/fog.svg"),
+  Overcast: require("@/assets/svg/overcast.svg"),
   "Clear day": require("@/assets/svg/clear-day.svg"),
+  Cloudy: require("@/assets/svg/overcast.svg"),
 };
 
 export default {
   map: weatherImgObject,
-  defaultSrc: require("@/assets/svg/thunderstorms-extreme-rain.svg"),
+  defaultSrc: require("@/assets/svg/not-available.svg"),
 };
